b5952e2269181daa88bf4a9e1f6952d96fadbc9d
cashely <290119516@qq.com>
2024-12-03 06:55:49 +0000

    fix: 添加cors组件 #main

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import expressWs from 'express-ws';
+import cors from 'cors';
 import routes from './routes';
 import response from './middleware/response';
 
@@ -10,6 +11,8 @@ import response from './middleware/response';
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(cors());
+// app.use(express.static('public'))
 // app.use(multer().fields([{ name: 'files[]' }]));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
