66163dcd6f2fe0f1833fae41de3b30f77283b54f
cashely <290119516@qq.com>
2024-12-03 10:07:50 +0000

    fix: 修改构建目录 #main

--- dist/api/index.js
+++ dist/api/index.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _main = _interopRequireDefault(require("./main.js"));
+var _main = _interopRequireDefault(require("../main.js"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 var app = (0, _main["default"])();
 var _default = exports["default"] = app;
--- dist/main.js
+++ dist/main.js
@@ -8,6 +8,7 @@ var _express = _interopRequireDefault(require("express"));
 var _morgan = _interopRequireDefault(require("morgan"));
 var _bodyParser = _interopRequireDefault(require("body-parser"));
 var _expressWs = _interopRequireDefault(require("express-ws"));
+var _cors = _interopRequireDefault(require("cors"));
 var _routes = _interopRequireDefault(require("./routes"));
 var _response = _interopRequireDefault(require("./middleware/response"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
@@ -15,6 +16,8 @@ function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default":
 
 var app = (0, _express["default"])();
 app.use((0, _morgan["default"])('tiny'));
+app.use((0, _cors["default"])());
+// app.use(express.static('public'))
 // app.use(multer().fields([{ name: 'files[]' }]));
 app.use(_bodyParser["default"].json());
 app.use(_bodyParser["default"].urlencoded({
